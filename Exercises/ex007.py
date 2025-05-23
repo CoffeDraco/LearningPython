c576afc0052e4763c0f39b757517043eb6339378
CoffeDraco <allanherculano2016@gmail.com>
2025-05-23 18:44:20 -0300

    Also showed the typed scores in the print

--- Exercises/ex007.py
+++ Exercises/ex007.py
@@ -2,4 +2,4 @@
 n1 = float(input('Type the first score: '))
 n2 = float(input('Type the second score: '))
 
-print('The average is {:.1f}'.format((n1+n2)/2))
+print('The average of {:.1f} and {:.1f} is {:.1f}.'.format(n1, n2, (n1+n2)/2))
